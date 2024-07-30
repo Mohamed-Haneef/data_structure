45e7bbed4c0d5935e2501fe4678b9f7655570024
Mohamed-Haneef <mohamedhaneef8444@gmail.com>
2024-07-30 23:24:45 +0530

    reversing a node

--- linked_list/reverse.c
+++ linked_list/reverse.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// the node structure
 struct ListNode{
 	int val;
 	struct ListNode *next; 
 };
 
+// creating up new nodes function
 struct ListNode *node(int var){
 	struct ListNode *n = (struct ListNode *)malloc(sizeof(struct ListNode));
 	n->val = var;
 	n->next = NULL;
-	printf("size of listnode : %ld \n", sizeof(struct ListNode));
 	return n;
 }
 
+// To reverse a linked list
 struct ListNode *reverse_node(struct ListNode *l){
 	struct ListNode* prev = NULL;
     struct ListNode* current = l;
@@ -27,24 +29,19 @@ struct ListNode *reverse_node(struct ListNode *l){
 	return prev;
 }
 
-struct ListNode *add_two_nodes(struct ListNode *l1, struct ListNode *l2){
-	//Need to complete
-}
-
+// usage:
 int main(){
+    
 	struct ListNode *l = node(2);
-    printf("Node 1: %d \n",l->val);
+    printf("Given Node 1: %d \n",l->val);
     l->next = node(4);
-    printf("Node 2: %d \n",l->next->val);
+    printf("Given Node 2: %d \n",l->next->val);
     l->next->next = node(3);
-    printf("Node 3: %d \n",l->next->next->val);
+    printf("Given Node 3: %d \n",l->next->next->val);
 
-	struct ListNode *p = node(4);
-    printf("Node 1: %d \n",p->val);
-    p->next = node(5);
-    printf("Node 2: %d \n",p->next->val);
-    p->next->next = node(6);
-    printf("Node 3: %d \n",p->next->next->val);
-
-	add_two_nodes(l, p);
+    struct ListNode *p = reverse_node(l);
+    while(p != NULL){
+        printf("Reversed Node : %d \n", p->val);
+        p = p->next;
+    }
 }
