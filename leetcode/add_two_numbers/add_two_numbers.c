746743f45fb7f982800b954269f3ae5bb6a0cb35
Mohamed-Haneef <mohamedhaneef8444@gmail.com>
2024-07-31 23:20:46 +0530

    Add two numbers solution String method need to be fixed

--- leetcode/add_two_numbers/add_two_numbers.c
+++ leetcode/add_two_numbers/add_two_numbers.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct ListNode{
 	int val;
@@ -24,11 +25,83 @@ struct ListNode *reverse_node(struct ListNode *l){
 		prev = current;
 		current = next;
 	}
+	printf("Got reverse node");
 	return prev;
 }
 
-struct ListNode *add_two_nodes(struct ListNode *l1, struct ListNode *l2){
-	//Need to complete
+// struct ListNode* add_two_nodes(struct ListNode* l1, struct ListNode* l2) {
+//     struct ListNode* current = NULL;
+//     struct ListNode* sum = NULL;
+//     char node1[128] = {0}; 
+//     char node2[128] = {0};
+
+//     printf("I am here\n");
+    
+//     l1 = reverse_node(l1);
+//     l2 = reverse_node(l2);
+    
+//     printf("After reversing\n");
+    
+//     while (l1 != NULL) {
+//         char temp1[8];
+//         sprintf(temp1, "%d", l1->val);
+//         strcat(node1, temp1);
+//         printf("node1 str: %s\n", node1);
+//         l1 = l1->next;
+//     }
+    
+//     while (l2 != NULL) {
+//         char temp2[8];
+//         sprintf(temp2, "%d", l2->val);
+//         strcat(node2, temp2);  
+//         printf("node2 str: %s\n", node2);
+//         l2 = l2->next;
+//     }
+    
+//     long long result = atoi(node1) + atoi(node2);
+
+//     char str[128];
+//     sprintf(str, "%lld", result);
+
+//     for (int i = 0; str[i] != '\0'; i++) {
+//         int digit = str[i] - '0';  
+//         struct ListNode* newNode = node(digit);
+//         if (sum == NULL) {
+//             sum = newNode;
+//             current = sum;
+//         } else {
+//             current->next = newNode;
+//             current = current->next;
+//         }
+//         printf("The final node: %d\n", newNode->val);
+//     }
+
+//     return reverse_node(sum); 
+// }
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* dummy = node(0);
+    struct ListNode* current = dummy;
+    int remainder = 0;
+
+    while (l1 != NULL || l2 != NULL || remainder != 0) {
+        int val1 = (l1 != NULL) ? l1->val : 0; 
+        int val2 = (l2 != NULL) ? l2->val : 0; 
+        int total = val1 + val2 + remainder;       
+        remainder = total / 10;                
+
+        struct ListNode* newNode = node(total % 10);
+        current->next = newNode;               
+        current = newNode;                  
+
+        if (l1 != NULL) l1 = l1->next;        
+        if (l2 != NULL) l2 = l2->next;         
+    }
+
+
+    struct ListNode* result = (dummy->next); 
+    free(dummy);                        
+    return result;
 }
 
 int main(){
